5bf98d0f5daf34fa79aaf6d666607c2a84c61ff5
fuad-266 <fuadabdala266@gmail.com>
2026-02-25 13:27:35 -0800

    eveything is working

--- intern-task1/src/main/java/com/example/intern_task1/HelloControler.java
+++
@@ -1,4 +0,0 @@
-package com.example.intern_task1;
-
-public class HelloControler {
-}
---
+++ intern-task1/src/main/java/com/example/intern_task1/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.intern_task1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello Spring Boot!";
+    }
+}
