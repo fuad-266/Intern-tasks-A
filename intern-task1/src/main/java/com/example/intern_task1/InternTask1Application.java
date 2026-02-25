930ef463e742fb7efcac145f839fe7b608f14fbb
fuad-266 <fuadabdala266@gmail.com>
2026-02-25 12:26:01 -0800

    first commit

---
+++ intern-task1/src/main/java/com/example/intern_task1/InternTask1Application.java
@@ -0,0 +1,13 @@
+package com.example.intern_task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternTask1Application.class, args);
+	}
+
+}
---
+++ intern-task1/src/test/java/com/example/intern_task1/InternTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intern_task1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternTask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
