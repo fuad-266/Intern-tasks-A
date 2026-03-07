b450703090db5452e30a7535dd0f1be9ad03e657
fuad-266 <fuadabdala266@gmail.com>
2026-03-07 12:00:26 -0800

    making the structure

---
+++ student-course-system/student-course-system/src/main/java/entity/Student.java
@@ -0,0 +1,27 @@
+package com.example.student_course_system.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private int age;
+
+    public Student() {
+    }
+}
+
