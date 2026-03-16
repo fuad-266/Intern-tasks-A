f5126ca7a7762cccf83797fdc7b3cad13e9538ce
fuad-266 <fuadabdala266@gmail.com>
2026-03-16 02:29:26 -0700

    added the list method

--- student-course-system/student-course-system/src/main/java/com/example/student_course_system/repository/StudentRepository.java
+++ student-course-system/student-course-system/src/main/java/com/example/student_course_system/repository/StudentRepository.java
@@ -2,9 +2,12 @@ package com.example.student_course_system.repository;
 
 import com.example.student_course_system.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
     boolean existsByEmail(String email);
 
+    List<Student> findByAge(int age);
 }
