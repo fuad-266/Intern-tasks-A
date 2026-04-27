95cd565a0763f86ec2d0304f8e69d38ba2e059af
fuad-266 <fuadabdala266@gmail.com>
2026-04-27 09:07:39 -0700

    still learning security

--- security/src/main/java/com/example/security/student.java
+++ security/src/main/java/com/example/security/student.java
@@ -1,4 +1,48 @@
 package com.example.security;
 
 public class student {
+
+    private int Id;
+    private String Name;
+    private int age;
+
+
+    public student(int age, String name, int id) {
+        this.age = age;
+        Name = name;
+        Id = id;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "student{" +
+                "Id=" + Id +
+                ", Name='" + Name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
--- security/src/main/java/com/example/security/studentController.java
+++ security/src/main/java/com/example/security/studentController.java
@@ -1,4 +1,7 @@
 package com.example.security;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class studentController {
 }
