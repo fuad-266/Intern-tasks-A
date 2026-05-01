ef78d69e45cdaada6f22afd9cf6fd985a68b388d
fuad-266 <fuadabdala266@gmail.com>
2026-05-01 11:31:17 -0700

    corrected the product entity

--- order-manegment/order-manegment/src/main/java/com/fuad/order_management/Entity/Product.java
+++ order-manegment/order-manegment/src/main/java/com/fuad/order_management/Entity/Product.java
@@ -1,4 +1,20 @@
 package com.fuad.order_management.Entity;
 
-public class Product {
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+public class Product extends BaseEntity {
+
+    private String name;
+
+    private BigDecimal price;
+
+    private Integer stock;
 }
