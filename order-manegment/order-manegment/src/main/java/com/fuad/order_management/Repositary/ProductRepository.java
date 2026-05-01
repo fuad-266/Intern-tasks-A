c3afbb4e53ece52ed11913a78b7f553ac443dac3
fuad-266 <fuadabdala266@gmail.com>
2026-05-01 12:45:09 -0700

    correcting the xm; file

--- order-manegment/order-manegment/src/main/java/com/fuad/order_management/Repositary/ProductRepository.java
+++ order-manegment/order-manegment/src/main/java/com/fuad/order_management/Repositary/ProductRepository.java
@@ -2,7 +2,7 @@ package com.fuad.order_management.Repositary;
 
 
 
-import com.fuad.ordermanagement.entity.Product;
+import com.fuad.order_management.Entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
