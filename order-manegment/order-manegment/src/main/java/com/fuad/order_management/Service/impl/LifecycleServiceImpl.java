3ef710d2f494799688c31f3ff6f329bc8f220b05
fuad-266 <fuadabdala266@gmail.com>
2026-05-01 13:07:05 -0700

    updated the lifecycle of service

--- order-manegment/order-manegment/src/main/java/com/fuad/order_management/Service/impl/LifecycleServiceImpl.java
+++ order-manegment/order-manegment/src/main/java/com/fuad/order_management/Service/impl/LifecycleServiceImpl.java
@@ -1,4 +1,63 @@
 package com.fuad.order_management.Service.impl;
 
-public class LifecycleServiceImpl {
+
+
+import com.fuad.order_management.Entity.Product;
+import com.fuad.order_management.Service.LifecycleService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class LifecycleServiceImpl implements LifecycleService {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    @Transactional
+    public void testEntityStates() {
+
+        System.out.println("========== TRANSIENT ==========");
+
+        Product product = new Product();
+        product.setName("Laptop");
+        product.setPrice(BigDecimal.valueOf(1200));
+        product.setStock(10);
+
+        System.out.println(product.getId());
+
+        System.out.println("========== PERSISTENT ==========");
+
+        entityManager.persist(product);
+
+        System.out.println(product.getId());
+
+        System.out.println("========== DIRTY CHECKING ==========");
+
+        product.setPrice(BigDecimal.valueOf(1500));
+
+        System.out.println("Price changed without save()");
+
+        System.out.println("========== FLUSH ==========");
+
+        entityManager.flush();
+
+        System.out.println("SQL executed");
+
+        System.out.println("========== DETACHED ==========");
+
+        entityManager.detach(product);
+
+        product.setName("Detached Product");
+
+        System.out.println("Detached object modified");
+
+        System.out.println("No UPDATE should happen");
+    }
 }
