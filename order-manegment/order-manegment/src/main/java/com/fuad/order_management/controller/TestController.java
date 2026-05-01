f91b71dec5de77fc8509d00c06242130be35f048
fuad-266 <fuadabdala266@gmail.com>
2026-05-01 13:22:36 -0700

    corrected the controler tesr

--- order-manegment/order-manegment/src/main/java/com/fuad/order_management/controller/TestController.java
+++ order-manegment/order-manegment/src/main/java/com/fuad/order_management/controller/TestController.java
@@ -1,4 +1,21 @@
 package com.fuad.order_management.controller;
 
+import com.fuad.order_management.Service.LifecycleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class TestController {
+
+    private final LifecycleService lifecycleService;
+
+    @GetMapping("/test")
+    public String testLifecycle() {
+
+        lifecycleService.testEntityStates();
+
+        return "Lifecycle test completed";
+    }
 }
