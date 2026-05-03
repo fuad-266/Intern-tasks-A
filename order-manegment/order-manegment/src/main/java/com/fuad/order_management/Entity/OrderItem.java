0a76d76a2581f29791e5ba7608bf595674c8a6c5
fuad-266 <fuadabdala266@gmail.com>
2026-05-03 09:34:21 -0700

    correct the orderitem files

--- order-manegment/order-manegment/src/main/java/com/fuad/order_management/Entity/OrderItem.java
+++ order-manegment/order-manegment/src/main/java/com/fuad/order_management/Entity/OrderItem.java
@@ -1,4 +1,24 @@
 package com.fuad.order_management.Entity;
 
-public class OrderItem {
+
+import jakarta.persistence.*;
+        import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Product product;
+
+    private Integer quantity;
+
+    private BigDecimal price;
 }
