c6a4ce020c0ee7ce8c8727ee688628daac7f83fc
fuad-266 <fuadabdala266@gmail.com>
2026-05-03 11:57:57 -0700

    make it work

--- order-manegment/order-manegment/src/main/java/com/fuad/order_management/Repositary/OrderRepository.java
+++ order-manegment/order-manegment/src/main/java/com/fuad/order_management/Repositary/OrderRepository.java
@@ -1,4 +1,10 @@
 package com.fuad.order_management.Repositary;
 
-public interface OrderRepository {
+
+
+import com.fuad.order_management.Entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
 }
