2679f6d9fb71d67046524f11dd196a650f265414
fuad-266 <fuadabdala266@gmail.com>
2026-05-03 14:51:18 -0700

    making join fech

--- order-manegment/order-manegment/src/main/java/com/fuad/order_management/Repositary/OrderRepository.java
+++ order-manegment/order-manegment/src/main/java/com/fuad/order_management/Repositary/OrderRepository.java
@@ -3,11 +3,20 @@ package com.fuad.order_management.Repositary;
 
 
 import com.fuad.order_management.Entity.Order;
+
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    List<Order> findAll();
+    @Query("""
+            SELECT o
+            FROM Order o
+            JOIN FETCH o.user
+            """)
+    List<Order> findAllWithUsers();
 }
