351b101f420335ac2661c58ffab3f81180bee95b
fuad-266 <fuadabdala266@gmail.com>
2026-05-03 09:31:21 -0700

    still correcting the oder

--- order-manegment/order-manegment/src/main/java/com/fuad/order_management/Entity/Order.java
+++ order-manegment/order-manegment/src/main/java/com/fuad/order_management/Entity/Order.java
@@ -1,4 +1,43 @@
 package com.fuad.order_management.Entity;
 
-public class Order {
+
+import com.fuad.order_management.Entity.enums.OrderStatus;
+import jakarta.persistence.*;
+        import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    private BigDecimal totalAmount;
+
+    @OneToMany(
+            mappedBy = "order",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<OrderItem> items = new ArrayList<>();
+
+    public void addItem(OrderItem item) {
+        items.add(item);
+        item.setOrder(this);
+    }
+
+    public void removeItem(OrderItem item) {
+        items.remove(item);
+        item.setOrder(null);
+    }
 }
