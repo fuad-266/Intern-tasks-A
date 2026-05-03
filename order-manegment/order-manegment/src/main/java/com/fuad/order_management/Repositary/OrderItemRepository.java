521deef82b0a31169960a60d67699a5c8ed4b2ad
fuad-266 <fuadabdala266@gmail.com>
2026-05-03 12:01:49 -0700

    created orderitem repository

--- order-manegment/order-manegment/src/main/java/com/fuad/order_management/Repositary/OrderItemRepository.java
+++ order-manegment/order-manegment/src/main/java/com/fuad/order_management/Repositary/OrderItemRepository.java
@@ -1,4 +1,10 @@
 package com.fuad.order_management.Repositary;
 
-public interface OrderItemRepository {
+
+
+import com.fuad.order_management.Entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
 }
