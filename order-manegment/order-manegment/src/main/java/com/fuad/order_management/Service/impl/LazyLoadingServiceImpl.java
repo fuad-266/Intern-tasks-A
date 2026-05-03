0a02438180f3ebfee8cdffd8e31ebc457c898cb3
fuad-266 <fuadabdala266@gmail.com>
2026-05-03 14:39:29 -0700

    correct the  service ipml lazy loading

--- order-manegment/order-manegment/src/main/java/com/fuad/order_management/Service/impl/LazyLoadingServiceImpl.java
+++ order-manegment/order-manegment/src/main/java/com/fuad/order_management/Service/impl/LazyLoadingServiceImpl.java
@@ -1,4 +1,35 @@
 package com.fuad.order_management.Service.impl;
 
-public class LazyLoadingServiceImpl {
+
+
+import com.fuad.order_management.Entity.Order;
+import com.fuad.order_management.Repositary.OrderRepository;
+import com.fuad.order_management.Service.LazyLoadingService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class LazyLoadingServiceImpl implements LazyLoadingService {
+
+    private final OrderRepository orderRepository;
+
+    @Override
+    @Transactional
+    public void testNPlusOne() {
+
+        List<Order> orders = orderRepository.findAll();
+
+        System.out.println("========== ORDERS LOADED ==========");
+
+        for (Order order : orders) {
+
+            System.out.println(order.getId());
+
+            System.out.println(order.getUser().getFirstName());
+        }
+    }
 }
