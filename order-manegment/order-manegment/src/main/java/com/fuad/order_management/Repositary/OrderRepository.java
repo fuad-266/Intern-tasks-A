01caf76c89778875ee3883ad40b38b590d3b1e30
fuad-266 <fuadabdala266@gmail.com>
2026-05-03 14:33:42 -0700

    testing controller

--- order-manegment/order-manegment/src/main/java/com/fuad/order_management/Repositary/OrderRepository.java
+++ order-manegment/order-manegment/src/main/java/com/fuad/order_management/Repositary/OrderRepository.java
@@ -5,6 +5,9 @@ package com.fuad.order_management.Repositary;
 import com.fuad.order_management.Entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    List<Order> findAll();
 }
--- order-manegment/order-manegment/src/main/java/com/fuad/order_management/controller/TestController.java
+++ order-manegment/order-manegment/src/main/java/com/fuad/order_management/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.fuad.order_management.controller;
 
 import com.fuad.order_management.Service.LifecycleService;
+import com.fuad.order_management.Service.RelationshipTestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     private final LifecycleService lifecycleService;
+    private final RelationshipTestService relationshipTestService;
 
     @GetMapping("/test")
     public String testLifecycle() {
@@ -17,5 +19,13 @@ public class TestController {
         lifecycleService.testEntityStates();
 
         return "Lifecycle test completed";
+    }
+
+    @GetMapping("/relationships")
+    public String testRelationships() {
+
+        relationshipTestService.testRelationships();
+
+        return "Relationship test completed";
     }
 }
