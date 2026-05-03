1d94140495e0027ac6d195fd0587bdb1b6173992
fuad-266 <fuadabdala266@gmail.com>
2026-05-03 10:55:35 -0700

    fix the problem and make it create table

--- order-manegment/order-manegment/src/main/java/com/fuad/order_management/Entity/User.java
+++ order-manegment/order-manegment/src/main/java/com/fuad/order_management/Entity/User.java
@@ -2,9 +2,10 @@ package com.fuad.order_management.Entity;
 
 
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
-
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "users")
 @Getter
 @Setter
 public class User extends BaseEntity {
