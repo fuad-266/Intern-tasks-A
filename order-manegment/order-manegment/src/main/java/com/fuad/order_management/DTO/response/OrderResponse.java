8029702b0734ad48d50df54f05011fdf89a2a54d
fuad-266 <fuadabdala266@gmail.com>
2026-05-04 14:37:00 -0700

    - Align with existing DTO layer architecture (OrderSummaryDTO, etc.)

--- order-manegment/order-manegment/src/main/java/com/fuad/order_management/DTO/response/OrderResponse.java
+++ order-manegment/order-manegment/src/main/java/com/fuad/order_management/DTO/response/OrderResponse.java
@@ -1,4 +1,22 @@
 package com.fuad.order_management.DTO.response;
 
+
+
+import com.fuad.order_management.Entity.enums.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class OrderResponse {
+
+    private Long id;
+
+    private BigDecimal totalAmount;
+
+    private OrderStatus status;
 }
