248769762412a30880cddf8bb96aa6f28614690e
fuad-266 <fuadabdala266@gmail.com>
2026-05-04 13:54:21 -0700

    - Provide base structure for query business logic execution

--- order-manegment/order-manegment/src/main/java/com/fuad/order_management/Service/impl/QueryServiceImpl.java
+++ order-manegment/order-manegment/src/main/java/com/fuad/order_management/Service/impl/QueryServiceImpl.java
@@ -1,4 +1,36 @@
 package com.fuad.order_management.Service.impl;
 
-public class QueryServiceImpl {
+
+
+import com.fuad.order_management.DTO.projection.OrderSummaryDTO;
+import com.fuad.order_management.Repositary.OrderRepository;
+import com.fuad.order_management.Service.QueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QueryServiceImpl implements QueryService {
+
+    private final OrderRepository orderRepository;
+
+    @Override
+    public void getOrderSummaries() {
+
+        List<OrderSummaryDTO> summaries =
+                orderRepository.getOrderSummaries();
+
+        for (OrderSummaryDTO dto : summaries) {
+
+            System.out.println(dto.getOrderId());
+
+            System.out.println(dto.getCustomerName());
+
+            System.out.println(dto.getTotalAmount());
+
+            System.out.println(dto.getItemCount());
+        }
+    }
 }
