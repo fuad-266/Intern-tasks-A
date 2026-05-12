ea3d32db9b7c5d3fd3f7c6a7e1587e2e2ff6cf28
fuad-266 <fuadabdala266@gmail.com>
2026-05-12 11:47:22 -0700

    Provide base structure for security configuration layer

--- securityOauth/src/main/java/com/oauth/securityOauth/securityconfig.java
+++ securityOauth/src/main/java/com/oauth/securityOauth/securityconfig.java
@@ -1,4 +1,18 @@
 package com.oauth.securityOauth;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class securityconfig {
-}
+    @Bean
+    public DefaultSecurityFilterChain securityFilterChain(HttpSecurity http){
+return http.build();
+
+}}
+
