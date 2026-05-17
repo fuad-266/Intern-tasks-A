94d3d1964ecb7feb8380d8e2e4610faa591cdc76
fuad-266 <fuadabdala266@gmail.com>
2026-05-17 09:33:31 -0700

    - Enable integration with Spring Security's UserDetails interface - Prepare structure for user authentication and authorization handling

--- security-project/security-project/src/main/java/com/security/security_project/security/CustomUserDetails.java
+++ security-project/security-project/src/main/java/com/security/security_project/security/CustomUserDetails.java
@@ -1,4 +1,55 @@
+
 package com.security.security_project.security;
 
-public class CustomUserDetails {
+import com.security.security_project.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<SimpleGrantedAuthority> getAuthorities() {
+
+        return user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
