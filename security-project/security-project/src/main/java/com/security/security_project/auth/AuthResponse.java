5d635f0a0d2ac7d1b7d469079521330d0c1d4caf
fuad-266 <fuadabdala266@gmail.com>
2026-05-18 12:00:21 -0700

    - Establish foundation for returning authentication tokens and user information - Prepare structure for handling successful login/registration responses

--- security-project/security-project/src/main/java/com/security/security_project/auth/AuthResponse.java
+++ security-project/security-project/src/main/java/com/security/security_project/auth/AuthResponse.java
@@ -1,4 +1,11 @@
 package com.security.security_project.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class AuthResponse {
+
+    private String accessToken;
 }
