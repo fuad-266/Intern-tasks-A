e06d5a8cb0a44853a364469dcd9080c2e21eb1db
fuad-266 <fuadabdala266@gmail.com>
2026-05-18 11:54:06 -0700

    - Establish foundation for token generation and validation logic - Prepare service layer for Spring Security integration with JWT authentication

--- security-project/security-project/src/main/java/com/security/security_project/jwt/JwtService.java
+++ security-project/security-project/src/main/java/com/security/security_project/jwt/JwtService.java
@@ -1,4 +1,67 @@
 package com.security.security_project.jwt;
 
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
 public class JwtService {
+
+    private static final String SECRET_KEY =
+            "mysec";
+
+    private final Key key =
+            Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
+    public String generateToken(String username) {
+
+        return Jwts.builder()
+
+                .setSubject(username)
+
+                .setIssuedAt(new Date())
+
+                .setExpiration(
+                        new Date(System.currentTimeMillis()
+                                + 1000 * 60 * 15)
+                )
+
+                .signWith(key, SignatureAlgorithm.HS256)
+
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token, String username) {
+
+        return username.equals(extractUsername(token))
+                && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+
+        return extractClaims(token)
+                .getExpiration()
+                .before(new Date());
+    }
+
+    private Claims extractClaims(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
 }
