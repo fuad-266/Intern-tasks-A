c9bd812065e46fcc8cbbf300a6bfc2c8b278c842
fuad-266 <fuadabdala266@gmail.com>
2026-05-19 14:02:13 -0700

    - Establish foundation for implementing token refresh business operations - Integrate with existing RefreshToken entity and RefreshTokenRepository

--- security-project/security-project/src/main/java/com/security/security_project/refreshToken/RefreshTokenService.java
+++ security-project/security-project/src/main/java/com/security/security_project/refreshToken/RefreshTokenService.java
@@ -1,4 +1,28 @@
 package com.security.security_project.refreshToken;
 
+
+import com.security.security_project.entity.RefreshToken;
+import com.security.security_project.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken createRefreshToken(User user) {
+
+        RefreshToken refreshToken = RefreshToken.builder()
+                .token(UUID.randomUUID().toString())
+                .expiryDate(LocalDateTime.now().plusDays(7))
+                .user(user)
+                .build();
+
+        return refreshTokenRepository.save(refreshToken);
+    }
 }
