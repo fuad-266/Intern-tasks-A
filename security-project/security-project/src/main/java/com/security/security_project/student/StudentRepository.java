22fd5ae2f84ad1a98a75066e061d5e89c24cefc6
fuad-266 <fuadabdala266@gmail.com>
2026-05-31 02:42:22 -0700

    - Establish data access layer for student domain model - Follows repository pattern established in security project architecture

--- security-project/security-project/src/main/java/com/security/security_project/student/StudentRepository.java
+++ security-project/security-project/src/main/java/com/security/security_project/student/StudentRepository.java
@@ -1,4 +1,7 @@
 package com.security.security_project.student;
 
-public class StudentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository
+        extends JpaRepository<Student, Long> {
 }
