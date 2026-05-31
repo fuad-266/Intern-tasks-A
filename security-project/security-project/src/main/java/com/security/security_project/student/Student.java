9a1f04b2e4af5c84b9692fd2d84e6e3c6acfa2a9
fuad-266 <fuadabdala266@gmail.com>
2026-05-31 02:33:57 -0700

    - Establish foundation for student domain model - Prepare for student-related features and authentication roles

--- security-project/security-project/src/main/java/com/security/security_project/student/Student.java
+++ security-project/security-project/src/main/java/com/security/security_project/student/Student.java
@@ -1,4 +1,27 @@
 package com.security.security_project.student;
 
+import com.security.security_project.user.User;
+import jakarta.persistence.*;
+        import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+
+    private String email;
+
+    private String department;
+
+    @OneToOne
+    private User user;
 }
