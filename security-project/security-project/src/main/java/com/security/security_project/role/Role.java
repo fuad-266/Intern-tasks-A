1722325e84ffbfde0424afc3f4eb7f00c694f9b4
fuad-266 <fuadabdala266@gmail.com>
2026-06-01 00:58:03 -0700

    correcting our project from the begining

--- security-project/security-project/src/main/java/com/security/security_project/role/Role.java
+++
@@ -1,8 +0,0 @@
-package com.security.security_project.role;
-
-public enum Role {
-
-    ROLE_ADMIN,
-    ROLE_INSTRUCTOR,
-    ROLE_STUDENT
-}
--- security-project/security-project/src/main/java/com/security/security_project/student/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.security.security_project.student;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository
-        extends JpaRepository<Student, Long> {
-}
