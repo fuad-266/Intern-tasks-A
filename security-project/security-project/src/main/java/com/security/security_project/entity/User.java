a046b4bed0a4fa43492a7f8060802e11eb5ee91f
fuad-266 <fuadabdala266@gmail.com>
2026-06-01 01:48:59 -0700

    making the user entity extend the base entity

--- security-project/security-project/src/main/java/com/security/security_project/entity/User.java
+++ security-project/security-project/src/main/java/com/security/security_project/entity/User.java
@@ -1,7 +1,20 @@
 package com.security.security_project.entity;
 
-import jakarta.persistence.*;
-        import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
@@ -10,9 +23,9 @@ import jakarta.persistence.*;
 @Builder
 @Entity
 @Table(name = "users")
-public class User extends BaseEntity {
+public class User extends BaseEntity implements UserDetails {
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String username;
 
     @Column(nullable = false, unique = true)
@@ -21,5 +34,47 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private String password;
 
+    /**
+     * Examples:
+     * ROLE_ADMIN
+     * ROLE_INSTRUCTOR
+     * ROLE_STUDENT
+     */
+    @Column(nullable = false)
     private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
