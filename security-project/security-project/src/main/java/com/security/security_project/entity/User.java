f0c3bd4dcdc1551d298e81df42ec7edf0474472d
fuad-266 <fuadabdala266@gmail.com>
2026-06-01 01:34:51 -0700

    - Enable user data representation for security project domain

--- security-project/security-project/src/main/java/com/security/security_project/entity/User.java
+++ security-project/security-project/src/main/java/com/security/security_project/entity/User.java
@@ -1,4 +1,25 @@
 package com.security.security_project.entity;
 
-public class User {
+import jakarta.persistence.*;
+        import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role;
 }
