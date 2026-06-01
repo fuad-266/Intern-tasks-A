e45926febd649ad902913c7b025894fa692ddf9b
fuad-266 <fuadabdala266@gmail.com>
2026-06-01 12:28:12 -0700

    correcting the innner files

--- security-project/security-project/src/main/java/com/security/security_project/security/CustomUserDetailsService.java
+++ security-project/security-project/src/main/java/com/security/security_project/security/CustomUserDetailsService.java
@@ -3,13 +3,10 @@ package com.security.security_project.security;
 import com.security.security_project.entity.User;
 import com.security.security_project.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -26,10 +23,6 @@ public class CustomUserDetailsService implements UserDetailsService {
                         new UsernameNotFoundException(
                                 "User not found: " + username));
 
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                List.of(new SimpleGrantedAuthority(user.getRole().name()))
-        );
+        return user;
     }
 }
