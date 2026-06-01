55f26b2994430484ca5cb8613671213b970af6ba
fuad-266 <fuadabdala266@gmail.com>
2026-06-01 01:40:58 -0700

    - Enable authentication against User entity from domain model - Provide foundation for credential validation and user principal creation

--- security-project/security-project/src/main/java/com/security/security_project/security/CustomUserDetailsService.java
+++ security-project/security-project/src/main/java/com/security/security_project/security/CustomUserDetailsService.java
@@ -1,4 +1,25 @@
 package com.security.security_project.security;
 
-public class CustomUserDetailsService {
+import com.security.security_project.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+        import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService
+        implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        return userRepository
+                .findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "User not found"));
+    }
 }
