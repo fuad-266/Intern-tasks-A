fc7b410be8d2da8acf38a3c3e66371316d4b425a
fuad-266 <fuadabdala266@gmail.com>
2026-06-02 20:19:45 -0700

    - Provide base structure for handling auth-related HTTP requests

--- security-project/security-project/src/main/java/com/security/security_project/controller/AuthController.java
+++ security-project/security-project/src/main/java/com/security/security_project/controller/AuthController.java
@@ -1,4 +1,47 @@
 package com.security.security_project.controller;
 
+import com.security.security_project.dto.auth.AuthResponse;
+import com.security.security_project.dto.auth.LoginRequest;
+import com.security.security_project.dto.auth.RefreshTokenRequest;
+import com.security.security_project.dto.auth.RegisterRequest;
+import com.security.security_project.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(
+            @Valid @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @Valid @RequestBody LoginRequest request
+    ) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<AuthResponse> refreshToken(
+            @Valid @RequestBody RefreshTokenRequest request
+    ) {
+        return ResponseEntity.ok(authService.refreshToken(request));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<AuthResponse> logout() {
+        return ResponseEntity.ok(authService.logout());
+    }
 }
