6c8cec9614b1fbdd3f0d7f2e1164b15108d2e607
fuad-266 <fuadabdala266@gmail.com>
2026-06-02 20:51:00 -0700

    Provide base structure for testing authentication and security flows

--- security-project/security-project/src/main/java/com/security/security_project/controller/TestController.java
+++ security-project/security-project/src/main/java/com/security/security_project/controller/TestController.java
@@ -1,4 +1,16 @@
 package com.security.security_project.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
 public class TestController {
+
+    @GetMapping("/me")
+    public String me(Authentication authentication) {
+        return "Authenticated as: " + authentication.getName();
+    }
 }
