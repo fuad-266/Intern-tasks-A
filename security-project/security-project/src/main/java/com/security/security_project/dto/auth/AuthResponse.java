b84acc2824a3686d5b11178b39d2e787b1dc0699
fuad-266 <fuadabdala266@gmail.com>
2026-06-02 20:14:53 -0700

    make it smart and responsive

--- security-project/security-project/src/main/java/com/security/security_project/dto/auth/AuthResponse.java
+++ security-project/security-project/src/main/java/com/security/security_project/dto/auth/AuthResponse.java
@@ -1,11 +1,19 @@
 package com.security.security_project.dto.auth;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class AuthResponse {
 
     private String accessToken;
+    private String refreshToken;
+    private String message;
 }
